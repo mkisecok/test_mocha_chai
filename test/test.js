e827ed7ec12251c09b7d7866085f37968fd48239
Mkisecok <mahmutkisecok@gmail.com>
2022-03-14 14:29:31 +0100

    docs: Clean code

--- test/test.js
+++ test/test.js
@@ -13,49 +13,47 @@ describe('ProjectTest;', () =>
         const wort = project.confirmEnding('mahmut', 'uts');
         const sentence = project.confirmEnding('I have not tested before', 'ed before');
 
-        if(sentence)
-        {
-            it('end of sentence passed', () =>{
-                expect(sentence)
-                .to.be.a('boolean')
-                .to.equal(true);
-            });
-        };
-        if(!wort)
-        {
-            it('end of wort not passed', () =>{
+            it('end of wort not passed', () =>
+            {
                 expect(wort)
-                .to.be.a('boolean')
-                .to.equal(false);
-            })
-        };
+                    .to.be.a('boolean')
+                    .to.equal(false);
+            });
+
+            it('end of sentence passed', () =>
+            {
+                expect(sentence)
+                    .to.be.a('boolean')
+                    .to.equal(true);
+            });
+       
         
     });
     describe('Factorial:', ()=> 
     {
-        const gtZero=project.factorial(5);
-        const eqZero=project.factorial(0);
-        const ltZero=project.factorial(-5);
+        const gtZero = project.factorial(5);
+        const eqZero = project.factorial(0);
+        const ltZero = project.factorial(-5);
 
         it('greater than zero', ()=>
         {
             expect(gtZero)
-            .to.be.a('number')
-            .to.equal(120);
+                .to.be.a('number')
+                .to.equal(120);
         });
 
         it('equal to zero', ()=>
         {
             expect(eqZero)
-            .to.be.a('number')
-            .to.equal(1);
+                .to.be.a('number')
+                .to.equal(1);
         });
 
         it('less than zero', ()=>
         {
             expect(ltZero)
-            .to.be.a('number')
-            .to.deep.equal(-1);
+                .to.be.a('number')
+                .to.deep.equal(-1);
         });
 
 
@@ -63,17 +61,17 @@ describe('ProjectTest;', () =>
 
     describe('Largest of four of all array', ()=>
     {
-        const arr=project.largestOfFour([[4, 5, 18, 3], [7, 8, 15, 45], [100, 152, 1, 36], [5, 1005, 12, 27]]);
+        const arr = project.largestOfFour([[4, 5, 18, 3], [7, 8, 15, 45], [100, 152, 1, 36], [5, 1005, 12, 27]]);
 
         it('largest index variable', ()=>
         {
             expect(arr)
-            .to.be.an('array')
-            .and.to.be.lengthOf(4)
-            .to.deep.equal([ 18, 45, 152, 1005 ])
+                .to.be.an('array')
+                .and.to.be.lengthOf(4)
+                .to.deep.equal([ 18, 45, 152, 1005 ]);
+            
             
         });
     });
 
 });
-
